89d1ef96162438d912b908ab3a2420916a63a9dd
Zach Zemokhol <45083180+SoyStillGood@users.noreply.github.com>
2019-11-29 20:45:20 -0500

    here we go

---
+++ clustering.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+import numpy as np
+from sklearn.datasets import load_wine
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cluster import KMeans
+from sklearn.linear_model import RANSACRegressor
+
+os.makedirs('plots/wine_cluster', exist_ok=True)
+
+
+wine = load_wine()
+#iris_df = pd.DataFrame(iris.data, columns=iris.feature_names)
+column_names = wine.feature_names
+
+#print(iris_df)
+
+y = wine.target
+X = wine.data
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.35)
+
+f1_scores = []
+error_rate = []
+
+for i in range(1, 98):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X_train, y_train)
+    y_predicted = knn.predict(X_test)
+    f1_scores.append(f1_score(y_test, y_predicted, average="macro"))
+    error_rate.append(np.mean(y_predicted != y_test))
+
+plt.plot(f1_scores, color='green', label='f1 score', linestyle='--')
+plt.plot(error_rate, color='red', label='error rate', linestyle='--')
+plt.xlabel('n neighbors parameter')
+plt.ylabel('f1_score/error_rate')
+plt.legend()
+plt.savefig('plots/wine_cluster/f1_scores-errors.png')
+plt.close()
+
+
+plt.scatter(X[:,0], X[:,1], c=y, cmap='rainbow')
+plt.xlabel('Spea1 Length', fontsize=15)
+plt.ylabel('Sepal Width', fontsize=15)
+#plt.show()
+
+
+km = KMeans(n_clusters = 3, n_jobs = 4, random_state=19)
+km1 = km.fit(X)
+centers = km.cluster_centers_
+new_labels = km.labels_
+fig, axes = plt.subplots(1, 2, figsize=(16,8))
+axes[0].scatter(X[:, 0], X[:, 1], c=y, cmap='gist_rainbow',edgecolor='k', s=150)
+axes[1].scatter(X[:, 0], X[:, 1], c=new_labels, cmap='flag',edgecolor='k', s=150)
+axes[0].set_xlabel('Sepal length', fontsize=18)
+axes[0].set_ylabel('Sepal width', fontsize=18)
+axes[1].set_xlabel('Sepal length', fontsize=18)
+axes[1].set_ylabel('Sepal width', fontsize=18)
+axes[0].tick_params(direction='in', length=10, width=5, colors='k', labelsize=20)
+axes[1].tick_params(direction='in', length=10, width=5, colors='k', labelsize=20)
+axes[0].set_title('Actual', fontsize=18)
+axes[1].set_title('Predicted', fontsize=18)
+plt.savefig('plots/wine_cluster/actual-predicted')
+plt.close()
---
+++ solving.py
@@ -0,0 +1,153 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import os
+from sklearn.datasets import load_wine
+
+
+wine = load_wine()
+wine_df = pd.DataFrame(wine.data, columns=wine.feature_names)
+print(wine.target)
+
+print(wine_df.columns)
+
+
+os.makedirs('fplots/seaborn_heatmap', exist_ok=True)
+
+sns.set()
+
+fig, ax = plt.subplots(figsize=(14,14))
+sns.heatmap(wine_df.corr(), annot=True, ax=ax, cmap='ocean', fmt='.2f', annot_kws={"size": 15}, linewidths=.05)
+ax.set_xticklabels(wine_df.columns, rotation=45)
+ax.set_yticklabels(wine_df.columns, rotation=45)
+fig.subplots_adjust(top=.75)
+plt.savefig('fplots/seaborn_heatmap/wine_heatmap.png')
+# plt.tight_layout()
+plt.close()
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Alcohol % by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="alcohol", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Alcohol %",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-boxplot-alcohol.png')
+
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Malic Acid by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="malic_acid", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Malic Acid",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-boxplot-malic-acid.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Ash by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="ash", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Ash",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-boxplot-ash.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Alcalinity of Ash by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="alcalinity_of_ash", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Alcalinity of Ash",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-boxplot-alc-ash.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Magnesium by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="magnesium", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Magenesium",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-boxplot-mag.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Proanthocyanins by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="proanthocyanins", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Proanthocyanins",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-boxplot-pro.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Flavanoids by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="flavanoids", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Flavanoids",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-boxplot-flavanoids.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Non Flavanoid Phenols by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="nonflavanoid_phenols", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Non Flavanoid Phenols",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-boxplot-NFP.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Total Phenols by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="total_phenols", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Total Phenols",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-TotalPhenols.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Color Intensity by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="color_intensity", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Color Intensity",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-color.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Hue by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="hue", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Hue",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-boxplot-hue.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Dilution by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="od280/od315_of_diluted_wines", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Dilution",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-dilution.png')
+
+f, (ax) = plt.subplots(1, 1, figsize=(12, 4))
+f.suptitle('Proline by Class', fontsize=14)
+
+sns.boxplot(x=wine.target, y="proline", data=wine_df,  ax=ax)
+ax.set_xlabel("Class",size = 12,alpha=0.8)
+ax.set_ylabel("Proline",size = 12,alpha=0.8)
+
+plt.savefig('fplots/seaborn_heatmap/wine-proline.png')
+
+wine_df.hist(bins=10, color='steelblue', edgecolor='black', linewidth=1.0,
+           xlabelsize=6, ylabelsize=6, grid=False)
+plt.xlim(-500, 500)
+plt.tight_layout(rect=(0, 0, 1.2, 1.2))
+plt.show()
+
